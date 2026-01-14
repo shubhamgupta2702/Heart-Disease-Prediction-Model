786b45f99031212e26466fe0110399d7618b29c6
Shubham Gupta <147174815+shubhamgupta2702@users.noreply.github.com>
2026-01-14 05:31:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ model = joblib.load("KNN_heart.pkl")
 scaler = joblib.load("scaler.pkl")
 expected_columns = joblib.load("columns.pkl")
 
-st.title("Heart Stroke Prediction by akarsh")
+st.title("Heart Stroke Prediction❤️")
 st.markdown("Provide the following details to check your heart stroke risk:")
 
 # Collect user input
@@ -63,3 +63,5 @@ if st.button("Predict"):
         st.error("⚠️ High Risk of Heart Disease")
     else:
         st.success("✅ Low Risk of Heart Disease")
+
+st.title("Made with ❤️ by Shubham.")
